e87f819dc69387df21e358639169d85dea4981c0
Kacper Stępień <kacper2007x48@gmail.com>
2023-02-19 17:50:53 +0100

    make mechanism of displaying choosen section

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,12 +1,45 @@
-const cardFront = document.getElementById('card-front');
-const cardBack = document.getElementById('card-back');
-
-const card = document.getElementById('card');
-
+"use strict";
+// Flipping cards ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var card = document.getElementById('card');
+var cardFront = document.getElementById('card-front');
+var cardBack = document.getElementById('card-back');
 function flipCard() {
-    card.classList.toggle('is-switched');
+    card.classList.toggle('is-flipped');
 }
-
 cardFront.addEventListener('click', flipCard);
-
 cardBack.addEventListener('click', flipCard);
+// Navigation of Sections ////////////////////////////////////////////////////////////////////////////////////////////////////////
+var mainMenuSection = document.getElementById('main-menu-section');
+var displayCardsSection = document.getElementById('display-cards-section');
+var addCardSection = document.getElementById('add-card-section');
+var deleteCardsSection = document.getElementById('delete-cards-section');
+var modifyCardsSection = document.getElementById('modify-cards-section');
+var navAddCardBtn = document.getElementById('nav-add-card-btn');
+var navStartPracticingBtn = document.getElementById('nav-start-practicing-btn');
+var navDeleteCardsBtn = document.getElementById('nav-delete-card-btn');
+var navModifyCardsBtn = document.getElementById('nav-modify-card-btn');
+var backMainPageBtn = document.getElementById('back-main-page-btn');
+function displayMainMenu() {
+    mainMenuSection.classList.remove('hidden-section-left');
+    addCardSection.classList.add('hidden-section-right');
+    displayCardsSection.classList.add('hidden-section-right');
+    deleteCardsSection.classList.add('hidden-section-right');
+    modifyCardsSection.classList.add('hidden-section-right');
+    backMainPageBtn.classList.add('hidden');
+}
+function displaySection(section) {
+    mainMenuSection.classList.add('hidden-section-left');
+    section.classList.remove('hidden-section-right');
+    backMainPageBtn.classList.remove('hidden');
+}
+navAddCardBtn.addEventListener('click', function () { return displaySection(addCardSection); });
+navStartPracticingBtn.addEventListener('click', function () { return displaySection(displayCardsSection); });
+navDeleteCardsBtn.addEventListener('click', function () { return displaySection(deleteCardsSection); });
+navModifyCardsBtn.addEventListener('click', function () { return displaySection(modifyCardsSection); });
+backMainPageBtn.addEventListener('click', displayMainMenu);
+// Card Class ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var Card = /** @class */ (function () {
+    function Card() {
+    }
+    return Card;
+}());
--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,1 +1,52 @@
-const 
+// Flipping cards ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const card = document.getElementById('card') as HTMLDivElement;
+const cardFront = document.getElementById('card-front') as HTMLDivElement;
+const cardBack = document.getElementById('card-back') as HTMLDivElement;
+
+function flipCard() {
+  card.classList.toggle('is-flipped');
+}
+
+cardFront.addEventListener('click', flipCard);
+cardBack.addEventListener('click', flipCard);
+
+
+// Navigation of Sections ////////////////////////////////////////////////////////////////////////////////////////////////////////
+const mainMenuSection = document.getElementById('main-menu-section') as HTMLDivElement;
+const displayCardsSection = document.getElementById('display-cards-section') as HTMLDivElement;
+const addCardSection = document.getElementById('add-card-section') as HTMLDivElement;
+const deleteCardsSection = document.getElementById('delete-cards-section') as HTMLDivElement;
+const modifyCardsSection = document.getElementById('modify-cards-section') as HTMLDivElement;
+
+const navAddCardBtn = document.getElementById('nav-add-card-btn') as HTMLElement;
+const navStartPracticingBtn = document.getElementById('nav-start-practicing-btn') as HTMLElement;
+const navDeleteCardsBtn = document.getElementById('nav-delete-card-btn') as HTMLElement;
+const navModifyCardsBtn = document.getElementById('nav-modify-card-btn') as HTMLElement;
+
+const backMainPageBtn = document.getElementById('back-main-page-btn') as HTMLElement;
+
+function displayMainMenu() {
+    mainMenuSection.classList.remove('hidden-section-left');
+    addCardSection.classList.add('hidden-section-right');
+    displayCardsSection.classList.add('hidden-section-right');
+    deleteCardsSection.classList.add('hidden-section-right');
+    modifyCardsSection.classList.add('hidden-section-right');
+    backMainPageBtn.classList.add('hidden');
+}
+
+function displaySection(section: HTMLDivElement) {
+    mainMenuSection.classList.add('hidden-section-left');
+    section.classList.remove('hidden-section-right');
+    backMainPageBtn.classList.remove('hidden');
+}
+
+navAddCardBtn.addEventListener('click', () => displaySection(addCardSection));
+navStartPracticingBtn.addEventListener('click', () => displaySection(displayCardsSection));
+navDeleteCardsBtn.addEventListener('click', () => displaySection(deleteCardsSection));
+navModifyCardsBtn.addEventListener('click', () => displaySection(modifyCardsSection));
+backMainPageBtn.addEventListener('click', displayMainMenu);
+
+// Card Class ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class Card {
+
+}
